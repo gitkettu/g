9ca291edddfcbc917ebad34f39f279091add0c59
gitkettu <gitkettu@gmail.com>
2021-10-20 09:17:34 +0000

    ok

--- BIOS_only/v3_afterChroot.sh
+++ BIOS_only/v3_afterChroot.sh
@@ -25,8 +25,8 @@ CHOST="x86_64-pc-linux-gnu"
 # profile used for building.
 
 USE="X savedconfig xinerama elogind suid"
-MAKEOPTS="-j3"
-VIDEO_CARDS="radeon"
+MAKEOPTS="-j2"
+VIDEO_CARDS=""
 INPUT_DEVICES="libinput synaptics"
 
 ACCEPT_LICENSE="*"
@@ -46,13 +46,10 @@ EOF
   command eselect profile set 1
 
   # "Updating @world set"
-  command emerge --ask --update --deep --newuse @world
-
-  # "Installing filesystem packages"
-  command emerge --ask $PACKAGES
+  command emerge --update --deep --newuse @world
 
   # "Configuring timezone"
-  command echo "$TIMEZONE" > /etc/timezone
+  command echo "Europe/Helsinki" > /etc/timezone
   command emerge --quiet --config sys-libs/timezone-data
 
   # "Writing /etc/locale.gen file"
@@ -60,20 +57,7 @@ EOF
 # All blank lines and lines starting with # are ignored.
 #en_US ISO-8859-1
  en_US.UTF-8 UTF-8
-#ja_JP.EUC-JP EUC-JP
-ja_JP.UTF-8 UTF-8
-#ja_JP EUC-JP
-#en_HK ISO-8859-1
-#en_PH ISO-8859-1
-#de_DE ISO-8859-1
-#de_DE@euro ISO-8859-15
-#es_MX ISO-8859-1
-#fa_IR UTF-8
-#fr_FR ISO-8859-1
-#fr_FR@euro ISO-8859-15
-#it_IT ISO-8859-1
 EOF
-
 
   command locale-gen
   command eselect locale set en_US.utf8
@@ -82,7 +66,7 @@ EOF
   command env-update && source /etc/profile && export PS1="(chroot) $PS1"
 
   # "Downloading Kernel Sources"
-  command emerge --ask sys-kernel/gentoo-sources
+  command emerge sys-kernel/gentoo-sources
 
   # "Cleaning Kernel source folder"
   command cd /usr/src/linux/
@@ -95,7 +79,7 @@ EOF
   command make install
 
   # "Installing genkernel"
-  command emerge --ask sys-kernel/genkernel
+  command emerge ask sys-kernel/genkernel
 
   cat << EOF > /etc/fstab
 # /etc/fstab: static file system information.
@@ -114,17 +98,17 @@ EOF
 # <fs>                  <mountpoint>    <type>          <opts>          <dump/pass>
 
 # NOTE: If your BOOT partition is ReiserFS, add the notail option to opts.
-${BOOT_PARTITION}               /boot           vfat            noauto,noatime  1 2
-${SWAP_PARTITION}               none            swap            sw              0 0
-${ROOT_PARTITION}               /               ext4           noatime         	0 1
+BOOT               /boot           vfat            noauto,noatime  1 2
+SWAP               none            swap            sw              0 0
+ROOT               /               ext4           noatime         	0 1
 EOF
 
   # "Setting Hostname"
-  command sed -i "s/localhost/$HOSTNAME/g" /etc/conf.d/hostname
+  command sed -i "s/localhost/gentoo/g" /etc/conf.d/hostname
   # "Creating /etc/conf.d/net"
   command touch /etc/conf.d/net
   # "Installing netifrc"
-  command emerge --ask --noreplace net-misc/netifrc
+  command emerge --noreplace net-misc/netifrc
   # "Setting network interfaces to activate at boot"
   command cd /etc/init.d
   command ln -s net.lo net.enp1s0
@@ -136,7 +120,6 @@ EOF
   # "Installing GRUB"
   command grub-install /dev/sda
   command grub-mkconfig -o /boot/grub/grub.cfg
-  # "Base installation has finished but there are still some steps that have to be done manual. GRUB install plz"
 }
 
 #exporting necessary functions and variables
@@ -144,28 +127,28 @@ export -f install_gentoo_chroot
 export -f #
 export -f command
 
-export LRED
-export GREEN
-export LCYAN
-export LBLUE
-export LPURPLE
-export DGRAY
-export NC
-
-export PACKAGES
-export TIMEZONE
-export CFLAGS
-export CPU_FLAGS
-export USE_FLAGS
-export MAKE_OPTIONS
-#export BIOS_PARTITION
-export BOOT_PARTITION
-export SWAP_PARTITION
-export ROOT_PARTITION
-export ROOT_FS_TYPE
-export MAKE_OPTIONS
-export HOSTNAME
-export TOOLS
+#export LRED
+#export GREEN
+#export LCYAN
+#export LBLUE
+#export LPURPLE
+#export DGRAY
+#export NC
+#
+#export PACKAGES
+#export TIMEZONE
+#export CFLAGS
+#export CPU_FLAGS
+#export USE_FLAGS
+#export MAKE_OPTIONS
+##export BIOS_PARTITION
+#export BOOT_PARTITION
+#export SWAP_PARTITION
+#export ROOT_PARTITION
+#export ROOT_FS_TYPE
+#export MAKE_OPTIONS
+#export HOSTNAME
+#export TOOLS
 
 #./get-latest-stage3
 install_gentoo_chroot
