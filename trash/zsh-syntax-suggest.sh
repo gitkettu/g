88075e21454ea438827bbf69dac420e468b0174a
gitkettu <gitkettu@gmail.com>
2022-02-28 21:52:34 +0200

    a

--- trash/zsh-syntax-suggest.sh
+++
@@ -1,6 +0,0 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
